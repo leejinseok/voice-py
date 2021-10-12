8acc9c06bc3859aff797fc4e08b535dad44b99d7
leejinseok <sonaky47@gmail.com>
2021-10-12 20:41:54 +0900

    record sucessfully

--- src/main.py
+++ src/main.py
@@ -1,13 +1,6 @@
 import speech_recognition
 from record import record
 
-# r = sr.Recognizer()
-# print(r)
-# harvard = sr.AudioFile('test.wav')
-# with harvard as source:
-#   audio = r.record(source)
-#   print(r.recognize_google(audio))
-
 def main():
   fileName = record()
   print(fileName)
--- src/record.py
+++ src/record.py
@@ -6,34 +6,30 @@ def record():
     FORMAT = pyaudio.paInt16
     CHANNELS = 2
     RATE = 44100
-    RECORD_SECONDS = 5
     WAVE_OUTPUT_FILENAME = "output.wav"
-
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
                     input=True,
                     frames_per_buffer=CHUNK)
-
-    print("* recording")
-
     frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    print("* done recording")
+    try:
+        while True:
+            data = stream.read(CHUNK)
+            frames.append(data)
+    except KeyboardInterrupt:
+        pass
 
     stream.stop_stream()
     stream.close()
-    p.terminate()
+    audio.terminate()
 
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+    sound_file = wave.open(WAVE_OUTPUT_FILENAME, "wb")
+    sound_file.setnchannels(CHANNELS)
+    sound_file.setsampwidth(audio.get_sample_size(FORMAT))
+    sound_file.setframerate(RATE)
+    sound_file.writeframes(b''.join(frames))
+    sound_file.close()
+    return WAVE_OUTPUT_FILENAME
+
